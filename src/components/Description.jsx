9384a3e205e22df93a94045e9b0c8ad9bfbfc0a1
Jonathan <jonathanthornton1997@gmail.com>
2020-09-18 14:15:59 -0700

    Updated desgin

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,77 +1,76 @@
-import React, {useState, useEffect} from 'react'
-import styled from 'styled-components'
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const StyledDescription = styled.div`
-font-family: 'DM Mono', monospace;
-display: flex;
-flex-direction: column;
-width: 75%;
-justify-content: center;
-align-self:center;
-text-align: justify;
-background-color: rgb(240, 240, 240);
-border-radius: 5px;
-box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
-margin: 2%;
-padding: 4%;
+  font-family: "DM Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 75%;
+  justify-content: center;
+  align-self: center;
+  text-align: justify;
+  background-color: rgb(240, 240, 240);
+  border-radius: 5px;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  margin: 2%;
+  padding: 4%;
 
-@media(max-width: 500px) {
+  @media (max-width: 500px) {
     width: 80%;
-    font-size: .9rem;
+    font-size: 0.9rem;
     padding: 8%;
     margin-bottom: 5%;
-    
-};
+  }
 
-h1 {
+  h1 {
     text-align: center;
     padding: 0;
     margin: 0;
 
-    @media(max-width: 500px){
-        font-size: 1.4rem;
-        margin-bottom: 2%;
-        width:100%;
+    @media (max-width: 500px) {
+      font-size: 1.4rem;
+      margin-bottom: 2%;
+      width: 100%;
     }
-}
+  }
 
-h3 {
+  h3 {
     margin-top: 0;
     padding: 0;
-    text-align:center;
-}
+    text-align: center;
+  }
 
-.whom{
-    text-align:center;
-    padding:0;
+  .whom {
+    text-align: center;
+    padding: 0;
     margin-top: 1%;
-}
+  }
 
-.details {
+  .details {
     @media (max-width: 500px) {
-        line-height: 1.4rem;
+      line-height: 1.4rem;
     }
-}
-
-`
+  }
+`;
 const StyledDescriptionContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-
-`
-export default function Description({explanation, title, copyright}){
-    // const {explanation} = props
-    return (
-        <StyledDescriptionContainer>
-        <StyledDescription className='descriptionContainer'>
-            <h1>{title}</h1>
-            <p className='whom'>by:</p>
-            <h3>{copyright}</h3>
-            <p className = 'details'>{explanation}</p>
-        </StyledDescription>
-        </StyledDescriptionContainer>
-    ) 
-    
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  min-height:100%;
+  height: 975px;
+`;
+export default function Description({ explanation, title, copyright }) {
+  // const {explanation} = props
+  return (
+    <StyledDescriptionContainer>
+      <StyledDescription className="descriptionContainer">
+        <h1>{title}</h1>
+        <p className="whom">by:</p>
+        <h3>{copyright}</h3>
+        <p className="details">{explanation}</p>
+      </StyledDescription>
+    </StyledDescriptionContainer>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,49 @@
-import React, {useState, useEffect} from 'react'
-import DatePicker from 'react-datepicker';
-import styled from 'styled-components'
-
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import styled from "styled-components";
 
 const StyledHeader = styled.div`
-font-family: 'DM Mono', monospace;
-box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
-width: 50%;
-display: flex;
-flex-direction: column;
-background-color: rgb(240, 240, 240);
-border-radius: 5px;
-padding: 2%;
+  font-family: "DM Mono", monospace;
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(240, 240, 240);
+  border-radius: 5px;
+  padding: 2%;
 
-
-@media(max-width: 500px) {
-        width: 80%;
-        margin: 5% 0;
-    };
-`
+  @media (max-width: 500px) {
+    width: 80%;
+    margin: 5% 0;
+  } ;
+`;
 const StyledHeaderContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+`;
 
-`
-
-export default function Header({date, selectDate, dateSetter, setSelectDate, formattedDate, title}){
-    // const {date} = props
-    return (
-        <StyledHeaderContainer >
-        <StyledHeader className = 'headerContainer'>
-            <h2>Date of Image: </h2>
-            <DatePicker onChange={dateSetter} placeholderText="Select a date"  selected = {selectDate} inputStyle={{textAlign: 'center'}}/>
-        </StyledHeader>
-        </StyledHeaderContainer>
-    )
+export default function Header({
+  date,
+  selectDate,
+  dateSetter,
+  setSelectDate,
+  formattedDate,
+  title,
+}) {
+  // const {date} = props
+  return (
+    <StyledHeaderContainer>
+      <StyledHeader className="headerContainer">
+        <h2>Date of Image: </h2>
+        <DatePicker
+          onChange={dateSetter}
+          placeholderText="Select a date"
+          selected={selectDate}
+          inputStyle={{ textAlign: "center", justifyContent: "center" }}
+        />
+      </StyledHeader>
+    </StyledHeaderContainer>
+  );
 }
-
